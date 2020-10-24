718a8a047ce7d2f03ebd2de3d64541cd6cb87859
Wissam Hanna <hannawissam@gmail.com>
2020-10-24 22:46:38 +0300

    Added the needed comments of both functions

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,10 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+#
+#The below function takes a matrix and then
+# it calculate the inverse and assign it to the variable m#
+#
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
@@ -10,7 +13,9 @@ makeCacheMatrix <- function(x = matrix()) {
     m <<- NULL
   }
   get <- function() x
+  #calculate the inverse function and assign it to the m variable defined in the parent function
   setinverse <- function(inverse) m <<- inverse
+  #return the m variable defined in the parent function which contains the inversed matrix
   getinverse <- function() m
   list(set = set,
        get = get,
@@ -24,6 +29,7 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   m <- x$getinverse()
+  # check if the variable m is null then return the cached variable
   if (!is.null(m)) {
     message("getting cached data")
     return(m)
